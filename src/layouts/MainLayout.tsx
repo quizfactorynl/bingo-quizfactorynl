a03525f40437cc7363002577db93a0aea25162e6
quizfactorynl <141273603+quizfactorynl@users.noreply.github.com>
2023-08-23 19:19:30 +0500

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -29,6 +29,15 @@ export default function MainLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content={keywords || "Bingo App"} />
         <link rel="icon" href="/favicon.ico" />
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M2K6Q5FN');</script>
+      <!-- End Google Tag Manager -->
+
       </Head>
       <main {...mainProps}>{children}</main>
     </>
