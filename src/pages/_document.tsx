a2a54af4a43e7fd33b807f6377492a38278b8bb2
quizfactorynl <141273603+quizfactorynl@users.noreply.github.com>
2023-08-28 20:03:21 +0500

    removed script tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,11 @@ export default function Document() {
     <Html lang="en">
       <Head > 
          <script id="gtm-script"  dangerouslySetInnerHTML={{ 
-          __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-             })(window,document,'script','dataLayer','GTM-M2K6Q5FN');</script>`
+             })(window,document,'script','dataLayer','GTM-M2K6Q5FN');`
         }}></script>
       </Head>
       <body>
