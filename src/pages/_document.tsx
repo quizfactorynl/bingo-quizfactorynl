d80cfb2b868ac984d347be6c37ddade933b0f9c2
quizfactorynl <141273603+quizfactorynl@users.noreply.github.com>
2023-08-28 19:39:55 +0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head > 
-         <Script id="gtm-script" strategy="beforeInteractive" dangerouslySetInnerHTML={{ __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-         })(window,document,'script','dataLayer','GTM-M2K6Q5FN');</script>`}}></Script>
+         <Script id="gtm-script"  dangerouslySetInnerHTML={{ 
+          __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+             })(window,document,'script','dataLayer','GTM-M2K6Q5FN');</script>`
+        }}></Script>
       </Head>
       <body>
         <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2K6Q5FN"
